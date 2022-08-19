f52f3d96aa49a5b2b156b1adb9d11ec97a682d99
mikelxk <mike.lxk@gmail.com>
2022-08-19 06:35:43 -0400

    doc: add style: prettierrc use toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: "https://mikelxk.com",
   build: {
-    format: "file"
+    format: "file",
   },
   markdown: {
-    syntaxHighlight: 'prism',
+    syntaxHighlight: "prism",
   },
-});
+})
