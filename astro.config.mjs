e8f33962344268eb792a007ba2d41e3eeb81b103
Xiaokai Li <mike.lxk@gmail.com>
2022-08-20 10:25:50 -0400

    sort imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
-import compress from "astro-compress";
-
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
