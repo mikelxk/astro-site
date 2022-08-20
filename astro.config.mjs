3f7003fd3dcf2e25da4e6f8d6dd7fd8d4a26f2e8
Xiaokai Li <mike.lxk@gmail.com>
2022-08-20 10:44:52 -0400

    preload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), robotsTxt(), compress(), sitemap()],
+  integrations: [tailwind(), mdx(), robotsTxt(),  sitemap()],
   site: "https://mikelxk.com",
   build: {
     format: "file"
