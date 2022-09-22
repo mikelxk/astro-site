f047879458c14e01859cadc093ea74e7f9554ba5
Xiaokai Li <mike.lxk@gmail.com>
2022-09-22 19:48:37 -0400

    up blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,22 @@
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import robotsTxt from "astro-robots-txt";
-import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), robotsTxt(),  sitemap()],
+  integrations: [tailwind(), mdx(), robotsTxt(), sitemap()],
   site: "https://mikelxk.com",
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
   build: {
-    format: "file"
+    format: "file",
   },
   markdown: {
-    syntaxHighlight: "prism"
-  }
-});
+    syntaxHighlight: "prism",
+  },
+})
