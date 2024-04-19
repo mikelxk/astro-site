5ffaacd5bb94bffba49a32bbdc6b4794ace85d95
mikelxk <mike.lxk@gmail.com>
2024-04-18 23:56:37 -0700

    update icon use prefetch fmt code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,19 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
 import { defineConfig } from "astro/config"
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   integrations: [
     tailwind({ config: { applyBaseStyles: false } }),
     mdx(),
+    icon(),
     robotsTxt(),
     sitemap(),
   ],
   site: "https://mikelxk.com/",
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
   build: {
     format: "file",
     build: {
