5a3c1b802e270a1ec40d8811a19ba384efcad61b
Xiaokai Li <mike.lxk@gmail.com>
2024-12-15 02:45:15 +0800

    add ress

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,17 @@
+import rss from "@astrojs/rss"
+import { getCollection } from "astro:content"
+import type { APIContext } from "astro"
+
+export async function GET(context: APIContext) {
+  const posts = await getCollection("blog")
+  return rss({
+    title: "Mike's Blog",
+    description: "Mike's Recollection on Coding, Design, and Life",
+    site: context.site!,
+    items: posts.map(post => ({
+      ...post.data,
+	  pubDate: post.data.publishDate,
+      link: `/blog/${post.id}/`,
+    })),
+  })
+}
