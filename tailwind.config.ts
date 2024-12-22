ca577da271e65065a005ebfc5bc15dbaeca017cd
Xiaokai Li <mike.lxk@gmail.com>
2024-12-22 10:25:45 +0800

    up cv

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import typography from "@tailwindcss/typography"
 import type { Config } from "tailwindcss"
 
 export default {
@@ -5,5 +6,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typography],
 } satisfies Config
