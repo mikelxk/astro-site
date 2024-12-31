92e19728c7398936fad25ae4835418a5960369e2
Xiaokai Li <mike.lxk@gmail.com>
2024-12-31 17:18:26 +0800

    run fmt

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,6 @@
 import rss from "@astrojs/rss"
-import { getCollection } from "astro:content"
 import type { APIContext } from "astro"
+import { getCollection } from "astro:content"
 
 export async function GET(context: APIContext) {
   const posts = await getCollection("blog")
