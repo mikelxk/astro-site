be6cce9fc7c738e131686c63b81ef62d75676262
MikeLxk <mike.lxk@gmail.com>
2025-01-27 05:46:33 +0000

    up tailwind v4

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,13 @@
 import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@tailwindcss/vite"
 import icon from "astro-icon"
 import robotsTxt from "astro-robots-txt"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    mdx(),
-    icon(),
-    robotsTxt(),
-    sitemap(),
-  ],
+  integrations: [mdx(), icon(), robotsTxt(), sitemap()],
   site: "https://mikelxk.com/",
   build: {
     format: "file",
@@ -22,10 +16,13 @@ export default defineConfig({
     syntaxHighlight: "shiki",
     shikiConfig: {
       themes: {
-        light: 'solarized-light',
-        dark: 'solarized-dark',
+        light: "solarized-light",
+        dark: "solarized-dark",
       },
       wrap: true,
     },
+  },
+  vite: {
+    plugins: [tailwind()],
   },
 })
