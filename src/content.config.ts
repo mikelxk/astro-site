978d490d71375ec6900e4dc7abf5841b91eca770
mikelxk <mike.lxk@gmail.com>
2026-02-28 09:22:36 -0800

    Update Astro dependencies to beta versions

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { glob } from "astro/loaders"
-import { defineCollection, z } from "astro:content"
+import { defineCollection } from "astro:content"
+import { z } from "astro/zod"
 
 const blog = defineCollection({
   // The ID is a slug generated from the path of the file relative to `base`
